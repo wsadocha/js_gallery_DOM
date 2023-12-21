5af10f6011e217214f25adf7248c3e3b27bdfbae
Wojtek <wojciechsadocha@gmail.com>
2023-12-21 10:10:15 +0100

    make build

---
+++ index.1e8cfb0d.js
@@ -0,0 +1,3 @@
+var e=document.querySelector("#thumbs"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){e.preventDefault();var a=e.target;("IMG"===a.tagName||"A"===a.tagName)&&function(e){if("A"===e.tagName){var a=e.href;t.src=a}else{var r=e.closest("a");t.src=r}}//# sourceMappingURL=index.1e8cfb0d.js.map
+(a)});
+//# sourceMappingURL=index.1e8cfb0d.js.map
